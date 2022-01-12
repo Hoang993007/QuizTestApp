6309f25b3f15afa56d657f6cb0802f9a7b653761
Anhle150199 <anhle150199@gmail.com>
2022-01-12 15:34:53 +0700

    edit detail quiz

--- front-end/src/pages/features/teacher/edit-quiz/index.tsx
+++ front-end/src/pages/features/teacher/edit-quiz/index.tsx
@@ -18,12 +18,8 @@ const EditQuiz: React.FC = () => {
   const [data, setData] = useState([
     { Id: '', Question: '', Answer1: '', Answer2: '', Answer3: '', Answer4: '', CorrectAnswer: '', Edit: '' },
   ]);
-  const [quizDetail, setQuizDetail] = useState({
-    name: '',
-    timeLimit: 0,
-    numberOfQuestion: 0,
-    description: '',
-  });
+  const [quizDetail, setQuizDetail] = useState({ ...quiz });
+  const quizName = useRef<any>();
 
   const getQuestions = async () => {
     try {
@@ -60,13 +56,9 @@ const EditQuiz: React.FC = () => {
       const quizSnap = await getDoc(quizRef);
       dispatch(handleManageQuiz({ id: quizSnap.id, ...quizSnap.data() }));
     };
-    setQuizDetail({
-      name: quiz.name,
-      timeLimit: quiz.timeLimit,
-      numberOfQuestion: quiz.numberOfQuestion,
-      description: quiz.description,
-    });
+
     quiz.id ? getQuestions() : getQuiz();
+    setQuizDetail({ ...quiz });
   }, [quiz]);
 
   const QuestionDelete = async (e: any, key: any) => {
@@ -78,9 +70,11 @@ const EditQuiz: React.FC = () => {
         const questDocRef = doc(db, DbsName.QUESTION, e);
         await deleteDoc(questDocRef);
         openCustomNotificationWithIcon(NOTIFICATION_TYPE.SUCCESS, 'Delete successfully!', '');
-        quizDetail.numberOfQuestion -= 1;
+
+        const newData = { ...quizDetail };
+        newData.numberOfQuestion = quizDetail.numberOfQuestion - 1;
         console.log(quizDetail.numberOfQuestion);
-        setQuizDetail(quizDetail);
+        await setQuizDetail({ ...newData });
       } catch (error) {
         openCustomNotificationWithIcon(NOTIFICATION_TYPE.ERROR, 'Error when delete questions!', '');
       }
@@ -112,6 +106,7 @@ const EditQuiz: React.FC = () => {
 
   const submitChange = async () => {
     try {
+      const quizDocRef = doc(db, DbsName.QUIZ, quiz.id);
       data.forEach(async (quest, index) => {
         if (quest.Id == 'new') {
           console.log(index);
@@ -138,7 +133,11 @@ const EditQuiz: React.FC = () => {
           quest.Id = newRow.id;
           quest.Edit = '0';
           setData([...data]);
-          openCustomNotificationWithIcon(NOTIFICATION_TYPE.SUCCESS, 'Update successfully', '');
+          // update quiz numberOfQuestion
+          await updateDoc(quizDocRef, {
+            numberOfQuestion: quizDetail.numberOfQuestion,
+          });
+          openCustomNotificationWithIcon(NOTIFICATION_TYPE.SUCCESS, 'Updated question successfully', '');
         } else if (quest.Edit == '1') {
           const questInfoDocRef = doc(db, DbsName.QUESTION, quest.Id);
           await updateDoc(questInfoDocRef, {
@@ -151,11 +150,49 @@ const EditQuiz: React.FC = () => {
           });
           quest.Edit = '0';
           setData([...data]);
-          openCustomNotificationWithIcon(NOTIFICATION_TYPE.SUCCESS, 'Update successfully', '');
+          openCustomNotificationWithIcon(NOTIFICATION_TYPE.SUCCESS, 'Updated question successfully', '');
         }
       });
+
+      // check input valid
+      if (quizDetail.timeLimit == 0) {
+        openCustomNotificationWithIcon(NOTIFICATION_TYPE.ERROR, `Time limit is valid`, '');
+        return;
+      }
+      if (quizDetail.name == '' || quizDetail.description == '') {
+        openCustomNotificationWithIcon(NOTIFICATION_TYPE.ERROR, `Name or description is valid`, '');
+        return;
+      }
+
+      //check name exist
+      let check = 0;
+      const checkQuizName = await getDocs(query(collection(db, DbsName.QUIZ), where('name', '==', quizDetail.name)));
+      checkQuizName.forEach((result: any) => {
+        if (result.id != '') {
+          check += 1;
+        }
+      });
+      if (check != 0) {
+        // update quiz detail
+        await updateDoc(quizDocRef, {
+          timeLimit: quizDetail.timeLimit,
+          description: quizDetail.description
+        });
+        openCustomNotificationWithIcon(NOTIFICATION_TYPE.ERROR, 'The quiz\'s name was not updated due to a duplicate !', '');
+        return;
+      }
+
+      // update quiz detail
+      await updateDoc(quizDocRef, {
+        name: quizDetail.name,
+        timeLimit: quizDetail.timeLimit,
+        description: quizDetail.description
+      });
+      const name = quizName.current;
+      name.innerHTML = `${quizDetail.name}`;
+      openCustomNotificationWithIcon(NOTIFICATION_TYPE.SUCCESS, 'Update successfully !', '');
     } catch (error) {
-      openCustomNotificationWithIcon(NOTIFICATION_TYPE.ERROR, 'Error in updating questions', '');
+      openCustomNotificationWithIcon(NOTIFICATION_TYPE.ERROR, 'Error in updating questions !', '');
     }
   };
 
@@ -165,54 +202,54 @@ const EditQuiz: React.FC = () => {
         <div className="title">
           <div className="name-test">
             <h2>
-              Edit Quiz: <strong>{quiz.name}</strong>
+              Edit Quiz: <strong ref={quizName}>{quiz.name}</strong>
               {/* <a className="icon-test" onClick={() => EditQuizDetail()}><AiTwotoneEdit color='#327a81' /></a> */}
             </h2>
           </div>
         </div>
-        <div className="detail-quiz">
+        {
+          quiz &&
+          <div className="detail-quiz">
+            <div className="element-detail">
+              <div className="form-label">Name:</div>
+              <input type="text" className="form-control" name="nameQuiz" id="nameQuiz"
+                defaultValue={quizDetail.name}
+                onChange={(e) => {
+                  quizDetail.name = e.target.value;
+                  setQuizDetail({ ...quizDetail });
+                }} />
+            </div>
 
-          <div className="element-detail">
-            <div className="form-label">Name:</div>
-            <input type="text" className="form-control" name="nameQuiz" id="nameQuiz"
-              defaultValue={quizDetail.name}
-              onChange={(e) => {
-                quizDetail.name = e.target.value;
-                setQuizDetail(quizDetail);
-                console.log(quizDetail);
+            <div className="element-detail">
+              <div className="form-label">Time Limit (minute):</div>
+              <input type="text" className="form-control"
+                value={quizDetail.timeLimit / 60}
+                onChange={(e) => {
+                  quizDetail.timeLimit = parseInt(e.target.value) * 60;
+                  if (isNaN(quizDetail.timeLimit)) quizDetail.timeLimit = 0;
+                  setQuizDetail({ ...quizDetail });
+                }}
+              />
+            </div>
 
-              }} />
+            <div className="element-detail">
+              <div className="form-label">Question Count:</div>
+              <input type="text" className="form-control" name="numberOfQuestion" id="numberOfQuestion"
+                value={quizDetail.numberOfQuestion} />
+            </div>
+
+            <div className="element-detail">
+              <div className="form-label">Description:</div>
+              <TextareaAutosize
+                defaultValue={quizDetail.description}
+                onChange={(e) => {
+                  quizDetail.description = e.target.value;
+                  setQuizDetail({ ...quizDetail });
+                }}
+              />
+            </div>
           </div>
-
-          <div className="element-detail">
-            <div className="form-label">Time Limit (second):</div>
-
-            <input type="text" className="form-control"
-              defaultValue={quizDetail.timeLimit}
-              onKeyPress={(event) => {
-                if (!/[0-9]/.test(event.key)) {
-                  event.preventDefault();
-                }
-              }}
-              onChange={(e) => { quizDetail.timeLimit = parseInt(e.target.value); setQuizDetail(quizDetail); }} />
-          </div>
-
-          <div className="element-detail">
-            <div className="form-label">Question Count:</div>
-            <input type="text" className="form-control" name="numberOfQuestion" id="numberOfQuestion"
-              value={quizDetail.numberOfQuestion} />
-          </div>
-          <div className="element-detail">
-            <div className="form-label">Description:</div>
-            <TextareaAutosize
-              value={quizDetail.description}
-              onChange={(e) => {
-                quizDetail.description = e.target.value;
-                setQuizDetail(quizDetail);
-              }}
-            />
-          </div>
-        </div>
+        }
         <table>
           <thead>
             <tr>
